3f468c99e20d1664a0b931a7ff96426df67fb6d4
Se Jin Lee <sjlee2016@sogang.ac.kr>
2020-09-18 04:22:20 +0900

    Update analyze.sh

--- mm_dst/analyze.sh
+++ mm_dst/analyze.sh
@@ -1,8 +1,9 @@
 
 PATH_DIR=$(realpath .)
 DOMAIN='furniture'
+#DOMAIN='fashion'
 python -m gpt2_dst.scripts.analysis \
-    --input_path_target="${PATH_DIR}"/gpt2_dst/data/${DOMAIN}/furniture_devtest_dials_target.txt \
-    --input_path_predicted="${PATH_DIR}"/gpt2_dst/results/${DOMAIN}/"${KEYWORD}"/${DOMAIN}_devtest_dials_predicted.txt \
-    --output_path_report="${PATH_DIR}"/gpt2_dst/results/${DOMAIN}/"${KEYWORD}"/"${DOMAIN}"_devtest_dials_analyze.json \
+    --input_path_target="${PATH_DIR}"/gpt2_dst/data/${DOMAIN}/${DOMAIN}_devtest_dials_target.txt \
+    --input_path_predicted="${PATH_DIR}"/gpt2_dst/results/${DOMAIN}/${DOMAIN}_devtest_dials_predicted.txt \
+    --output_dir="${PATH_DIR}"/gpt2_dst/results/${DOMAIN}/analysis \
     --limit=0.1
