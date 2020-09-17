dfcf1ad23236b2663f8a93180621e314ec399299
Se Jin Lee <sjlee2016@sogang.ac.kr>
2020-09-18 05:46:16 +0900

    Update and rename analyze.sh to run_analyze_gpt.sh

--- mm_dst/analyze.sh
+++
@@ -1,9 +0,0 @@
-
-PATH_DIR=$(realpath .)
-DOMAIN='furniture'
-#DOMAIN='fashion'
-python -m gpt2_dst.scripts.analysis \
-    --input_path_target="${PATH_DIR}"/gpt2_dst/data/${DOMAIN}/${DOMAIN}_devtest_dials_target.txt \
-    --input_path_predicted="${PATH_DIR}"/gpt2_dst/results/${DOMAIN}/${DOMAIN}_devtest_dials_predicted.txt \
-    --output_dir="${PATH_DIR}"/gpt2_dst/results/${DOMAIN}/analysis \
-    --limit=0.1
