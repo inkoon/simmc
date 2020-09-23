19d23779c4a7d04184eb6568e19ad8777fb9085f
Se Jin Lee <sjlee2016@sogang.ac.kr>
2020-09-23 21:44:17 +0900

    fix pretrain script

---
+++ mm_dst/run_pretrain_gpt2.sh
@@ -0,0 +1,38 @@
+PATH_DIR=$(realpath .)
+# PARAMETERS	DEFAUT VALUE
+EPOCHS=1	# 1
+TRAIN_BATCH=4	# 4
+VAL_BATCH=32	# 4
+MUL_GPU=0	# 1
+GPU_ID='2'	# '0'
+WARM_UP=2000	# 0
+LR=5e-5		# 5e-5
+LOGGING=100	# 500
+N_GPU=1
+PARAMETERS="EPOCHS		$EPOCHS
+TRAIN_BATCH	$TRAIN_BATCH
+VAL_BATCH	$VAL_BATCH
+MUL_GPU		$MUL_GPU
+N_GPU 		$N_GPU
+"
+
+python -m gpt2_dst.scripts.run_language_modeling \
+    --output_dir="${PATH_DIR}"/gpt2_dst/save/fine_tune/ \
+    --model_type=gpt2 \
+    --model_name_or_path=gpt2 \
+    --line_by_line \
+    --do_train \
+    --train_data_file=/home/sejin/simmc/mm_dst/schema_data/output_train.txt \
+    --do_eval \
+    --eval_data_file=/home/sejin/simmc/mm_dst/schema_data/output_dev.txt \
+    --evaluate_during_training \
+    --logging_steps=$LOGGING \
+    --num_train_epochs=$EPOCHS \
+    --overwrite_output_dir \
+    --learning_rate=$LR \
+    --warmup_steps=$WARM_UP \
+    --gpu_id=$GPU_ID \
+    --mul_gpu=$MUL_GPU \
+    --n_gpu=$N_GPU \
+    --per_gpu_train_batch_size=$TRAIN_BATCH \
+    --per_gpu_eval_batch_size=$VAL_BATCH
