6148224759ef5c07ee147792a7c0b14b3fd95c39
boychaboy <hoon2j@gmail.com>
2020-09-23 23:43:43 +0900

    use output total for input file

--- mm_dst/run_pretrain_gpt2.sh
+++ mm_dst/run_pretrain_gpt2.sh
@@ -1,14 +1,13 @@
 PATH_DIR=$(realpath .)
 # PARAMETERS	DEFAUT VALUE
 EPOCHS=1	# 1
-TRAIN_BATCH=4	# 4
+TRAIN_BATCH=24	# 4
 VAL_BATCH=32	# 4
 MUL_GPU=0	# 1
-GPU_ID='2'	# '0'
+GPU_ID='1'	# '0'
 WARM_UP=2000	# 0
 LR=5e-5		# 5e-5
-LOGGING=100	# 500
-N_GPU=1
+LOGGING=3000	# 500:
 PARAMETERS="EPOCHS		$EPOCHS
 TRAIN_BATCH	$TRAIN_BATCH
 VAL_BATCH	$VAL_BATCH
@@ -22,9 +21,9 @@ python -m gpt2_dst.scripts.run_language_modeling \
     --model_name_or_path=gpt2 \
     --line_by_line \
     --do_train \
-    --train_data_file=/home/sejin/simmc/mm_dst/schema_data/output_train.txt \
+    --train_data_file="${PATH_DIR}"/schema_data/output_total.txt \
     --do_eval \
-    --eval_data_file=/home/sejin/simmc/mm_dst/schema_data/output_dev.txt \
+    --eval_data_file="${PATH_DIR}"/schema_data/output_dev.txt \
     --evaluate_during_training \
     --logging_steps=$LOGGING \
     --num_train_epochs=$EPOCHS \
@@ -33,6 +32,6 @@ python -m gpt2_dst.scripts.run_language_modeling \
     --warmup_steps=$WARM_UP \
     --gpu_id=$GPU_ID \
     --mul_gpu=$MUL_GPU \
-    --n_gpu=$N_GPU \
+    --fp16 \
     --per_gpu_train_batch_size=$TRAIN_BATCH \
     --per_gpu_eval_batch_size=$VAL_BATCH
