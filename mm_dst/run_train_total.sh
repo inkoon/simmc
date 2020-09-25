bc9220ecf36547cf3b8c1b4e4922dfc552ee410a
boychaboy <hoon2j@gmail.com>
2020-09-25 10:53:07 +0900

    total_large_total running

--- mm_dst/run_train_total.sh
+++ mm_dst/run_train_total.sh
@@ -15,7 +15,7 @@ then
 	VERSION=$3
 fi
 
-GPU_ID=0
+GPU_ID=1
 MUL_GPU=0
 NUM_GEN=100000
 
@@ -60,18 +60,18 @@ python -m gpt2_dst.scripts.preprocess_input \
 # Train ("${DOMAIN}", multi-modal)
 python -m gpt2_dst.scripts.run_language_modeling \
     --output_dir="${PATH_DIR}"/gpt2_dst/save/"${DOMAIN}"/"${KEYWORD}""${VERSION}" \
-    --model_type=gpt2 \
-    --model_name_or_path=gpt2 \
+    --model_type=gpt2-large \
+    --model_name_or_path=gpt2-large \
     --line_by_line \
-    --add_special_tokens="${PATH_DIR}"/gpt2_dst/data/"${DOMAIN}"_"${KEYWORD}"/special_tokens.json \
+    --add_special_tokens="${PATH_DIR}"/gpt2_dst/data/"${DOMAIN}"/special_tokens.json \
     --do_train \
-    --train_data_file="${PATH_DIR}"/gpt2_dst/data/"${DOMAIN}"_"${KEYWORD}"/"${DOMAIN}"_total_dials_target.txt \
-    --num_train_epochs=10 \
+    --train_data_file="${PATH_DIR}"/gpt2_dst/data/"${DOMAIN}"/"${DOMAIN}"_total_dials_target.txt \
+    --num_train_epochs=5 \
     --overwrite_output_dir \
     --gpu_id=$GPU_ID \
-    --per_gpu_train_batch_size=8 \
-    --per_gpu_eval_batch_size=32 \
-    --warmup_steps=4000 \
+    --per_gpu_train_batch_size=2 \
+    --per_gpu_eval_batch_size=8 \
+    --warmup_steps=8000 \
     --logging_steps=0 \
     --save_steps=0 \
     --fp16 
@@ -87,9 +87,22 @@ CUDA_VISIBLE_DEVICES=$GPU_ID python -m gpt2_dst.scripts.run_generation \
     --stop_token="<EOS>" \
     --num_beams=2 \
     --num_gen=$NUM_GEN \
-    --prompts_from_file="${PATH_DIR}"/gpt2_dst/data/"${DOMAIN}"_"${KEYWORD}"/"${DOMAIN}"_devtest_dials_predict.txt \
+    --prompts_from_file="${PATH_DIR}"/gpt2_dst/data/"${DOMAIN}"/"${DOMAIN}"_devtest_dials_predict.txt \
     --path_output="${PATH_DIR}"/gpt2_dst/results/"${DOMAIN}"/"${KEYWORD}""${VERSION}"/"${DOMAIN}"_devtest_dials_predicted.txt
 
+python -m gpt2_dst.utils.total_postprocess \
+    --path="${PATH_DIR}"/gpt2_dst/results/"${DOMAIN}"/"${KEYWORD}""${VERSION}"/"${DOMAIN}"_devtest_dials_predicted.txt \
+    --domain="${DOMAIN}"
+
+mv "${PATH_DIR}"/gpt2_dst/results/"${DOMAIN}"/"${KEYWORD}""${VERSION}"/"${DOMAIN}"_devtest_dials_predicted.txt "${PATH_DIR}"/gpt2_dst/results/"${DOMAIN}"/"${KEYWORD}""${VERSION}"/"${DOMAIN}"_devtest_dials_predicted.org
+
+mv "${PATH_DIR}"/gpt2_dst/results/"${DOMAIN}"/"${KEYWORD}""${VERSION}"/"${DOMAIN}"_devtest_dials_predicted_processed.txt "${PATH_DIR}"/gpt2_dst/results/"${DOMAIN}"/"${KEYWORD}""${VERSION}"/"${DOMAIN}"_devtest_dials_predicted.txt
+
+# Evaluate ("${DOMAIN}, multi-modal)
+python -m gpt2_dst.scripts.evaluate \
+    --input_path_target="${PATH_DIR}"/gpt2_dst/data/"${DOMAIN}"/"${DOMAIN}"_devtest_dials_target.txt \
+    --input_path_predicted="${PATH_DIR}"/gpt2_dst/results/"${DOMAIN}"/"${KEYWORD}""${VERSION}"/"${DOMAIN}"_devtest_dials_predicted.txt \
+ 
 '
 # Evaluate ("${DOMAIN}, multi-modal)
 python -m gpt2_dst.scripts.evaluate \
