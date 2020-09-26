32c4d0aa22f0c3614b37f69b38d02813c7d20d7b
boychaboy <hoon2j@gmail.com>
2020-09-26 09:52:36 +0900

    nipa2

--- mm_dst/to_total_train.sh
+++ mm_dst/to_total_train.sh
@@ -15,7 +15,7 @@ then
 	VERSION=$3
 fi
 
-GPU_ID=1
+GPU_ID=0
 
 PATH_DIR=$(realpath .)
 
--- mm_dst/total_generate.sh
+++ mm_dst/total_generate.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-if [[ $# -eq 0 ]] || [[ $# -eq 1 ]]
+if [[ $# -eq 0 ]] || [[ $# -eq 1 ]] || [[ $# -eq 2 ]]
 then
 	echo "run format > ./run_domain_keyword.sh [domain] [keyword]"
 	exit 1
-elif [[ $# -eq 2 ]]
-then
-	DOMAIN=$1
-	KEYWORD=$2
-	VERSION=""
+
 elif [[ $# -eq 3 ]]
 then
 	DOMAIN=$1
 	KEYWORD=$2
-	VERSION=$3
+	GPU_ID=$3
 fi
-
-GPU_ID=0
 
 PATH_DIR=$(realpath .)
 
@@ -25,7 +19,7 @@ CUDA_VISIBLE_DEVICES=$GPU_ID python -m gpt2_dst.scripts.run_generation \
     --model_name_or_path="${PATH_DIR}"/gpt2_dst/save/"${DOMAIN}"/"${KEYWORD}""${VERSION}" \
     --num_return_sequences=1 \
     --length=100 \
-    --gpu_id=$GPU_ID \
+    --gpu_id="{$GPU_ID}" \
     --stop_token="<EOS>" \
     --num_beams=2 \
     --prompts_from_file="${PATH_DIR}"/gpt2_dst/data/"${DOMAIN}"/"${DOMAIN}"_devtest_dials_predict.txt \
