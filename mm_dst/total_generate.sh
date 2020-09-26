c719cfe90fad060ec7822a39b8e7be9a170b8ffe
boychaboy <hoon2j@gmail.com>
2020-09-26 12:03:46 +0900

    230

--- mm_dst/total_generate.sh
+++ mm_dst/total_generate.sh
@@ -11,17 +11,19 @@ then
 	GPU_ID=$3
 fi
 
+VERSION=_p
 PATH_DIR=$(realpath .)
 
 # Generate sentences ("${DOMAIN}", multi-modal)
 CUDA_VISIBLE_DEVICES=$GPU_ID python -m gpt2_dst.scripts.run_generation \
     --model_type=gpt2 \
-    --model_name_or_path="${PATH_DIR}"/gpt2_dst/save/"${DOMAIN}"/"${KEYWORD}""${VERSION}" \
+    --model_name_or_path="${PATH_DIR}"/gpt2_dst/save/"${DOMAIN}"/"${KEYWORD}" \
     --num_return_sequences=1 \
     --length=100 \
-    --gpu_id="{$GPU_ID}" \
+    --gpu_id=$GPU_ID \
     --stop_token="<EOS>" \
     --num_beams=2 \
+    --p=0.5 \
     --prompts_from_file="${PATH_DIR}"/gpt2_dst/data/"${DOMAIN}"/"${DOMAIN}"_devtest_dials_predict.txt \
     --path_output="${PATH_DIR}"/gpt2_dst/results/"${DOMAIN}"/"${KEYWORD}""${VERSION}"/"${DOMAIN}"_devtest_dials_predicted.txt
 
